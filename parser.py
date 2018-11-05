19042ebc6aa3fa6afda37fe532dc6a2bdda35e38
Gustavo Mendonca <gustavogmend@gmail.com>
2018-11-05 20:20:12 -0200

    Adding argparse to parser

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ import re
 import time
 import logging
 import socket
+import argparse
 from datetime import datetime, timedelta, date
 from elasticsearch import Elasticsearch
 
@@ -12,8 +13,8 @@ class Config(object):
     GLOB_PATTERN = 'hadoop-cmf-CD-HIVE-*-HIVESERVER2-*.ec2.internal.log.out*'
     FROM_TIME = (datetime.today() - timedelta(minutes=15))
     TO_TIME = datetime.today().now()
-    ES_HOST = os.environ['ES_HOST']
-    ES_PORT = os.environ['ES_PORT']
+    ES_HOST = 'localhost'
+    ES_PORT = 9200
 
 class Parse(object):
 
@@ -128,8 +129,28 @@ class Parse(object):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", help="turn on logging level to DEBUG",
+                    action="store_true")
+    parser.add_argument("--es-host", help="Elasticsearch Host to send the logs to")
+    parser.add_argument("--es-port", type=int, help="Elasticsearch Port to send the logs to")
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
     config = Config()
+    if args.es_host:
+        config.ES_HOST = args.es_host
+
+    if args.es_port:
+        config.ES_PORT = args.es_port
+
+    logging.debug('Using ES {}:{}'.format(config.ES_HOST, config.ES_PORT))
+
     parse = Parse(config)
 
     parse.parse_logs()
